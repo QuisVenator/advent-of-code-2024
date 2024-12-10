a968f9a9dc40ad417054a395d93bcac5703368f0
Manuel René Pauls Toews <paulsmrene@live.com>
2024-12-10 16:05:28 -0300

    Solve day 10

--- day10.py
+++ day10.py
@@ -24,14 +24,34 @@ def hike(map, x, y, breadcumbs, bread_index):
     
     return score
 
+def rate(map, x, y):
+    if map[x][y] == 9:
+        return 1
+    
+    score = 0
+    if x > 0 and map[x-1][y] == map[x][y] + 1:
+        score += rate(map, x-1, y)
+    if x < len(map) - 1 and map[x+1][y] == map[x][y] + 1:
+        score += rate(map, x+1, y)
+    if y > 0 and map[x][y-1] == map[x][y] + 1:
+        score += rate(map, x, y-1)
+    if y < len(map[0]) - 1 and map[x][y+1] == map[x][y] + 1:
+        score += rate(map, x, y+1)
+    
+    return score
+
 result1 = 0
+result2 = 0
 counter = 0
 for i in range(len(map)):
     for j in range(len(map[0])):
         counter += 1
         if map[i][j] == 0:
             score = hike(map, i, j, breadcumbs, counter)
-            # print(f"Start at {i}, {j}, score: {score}")
+            rating = rate(map, i, j)
+            print(f"Start at {i}, {j}, score: {score}, rating: {rating}")
             result1 += score
+            result2 += rating
 
 print(f"Part 1: {result1}")
+print(f"Part 2: {result2}")
