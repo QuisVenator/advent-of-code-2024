2a9d76b7b5a87ba257ac41ec578cf5a116baaecb
Manuel René Pauls Toews <paulsmrene@live.com>
2024-12-17 08:58:08 -0300

    cleanup

--- day13.py
+++ day13.py
@@ -9,8 +9,6 @@ lines = [[int(x) for x in line] for line in lines]
 print(len(lines))
 
 def solve_multi_possible(A, B, P):
-    min_cost = float("inf")
-    
     # We have two basic cases with three subcases each:
     # 1. The A button moves us so much, that using it is cheaper than using the B button (meaning A > 3*B)
     #   1.1 We can reach the price without ever using the B button
@@ -39,7 +37,6 @@ def solve_multi_possible(A, B, P):
                 return 0
     # Case 2
     else:
-        print("Case 2")
         # Case 2.1
         if P[0] % B[0] == 0 and P[1] % B[1] == 0:
             if P[0] // B[0] == P[1] // B[1]:
@@ -48,13 +45,9 @@ def solve_multi_possible(A, B, P):
                 return 0
         # Case 2.2
         else:
-            print("Case 2.2")
-            print(f"{P[0] % B[0]} == {A[0]} and {P[1] % B[1]} == {A[1]}")
             if P[0] % B[0] == A[0] and P[1] % B[1] == A[1]:
-                print("Case 2.2 with solution")
                 return P[0] // B[0] + 3
             else:
-                print("Case 2.2 without solution")
                 return 0
 
 def solve_efficient(A, B, P):
@@ -73,19 +66,15 @@ def solve_efficient(A, B, P):
         m = (P[0] - n * B[0]) // A[0]
         return 3 * m + n
 
-total_cost = 0
-# for i in range(0, len(lines), 3):
-#     print(lines[i], lines[i+1], lines[i+2])
+total_cost_p1 = 0
+total_cost_p2 = 0
+for i in range(0, len(lines), 3):
+    res = solve_efficient(lines[i], lines[i+1], [lines[i+2][0], lines[i+2][1]])
+    total_cost_p1 += res
 
-#     conversion_error = 10000000000000 #* 0
+    conversion_error = 10000000000000
+    res = solve_efficient(lines[i], lines[i+1], [lines[i+2][0] + conversion_error, lines[i+2][1] + conversion_error])
+    total_cost_p2 += res
 
-#     res = solve_efficient(lines[i], lines[i+1], [lines[i+2][0] + conversion_error, lines[i+2][1] + conversion_error])
-#     # res_test = solve(lines[i], lines[i+1], [lines[i+2][0] + conversion_error, lines[i+2][1] + conversion_error])
-#     # if res != res_test:
-#     #     print(f"Error: {res} != {res_test} for {lines[i]}, {lines[i+1]}, {lines[i+2]}")
-#     total_cost += res
-#     print(i//3)
-
-print(solve_efficient([2,3], [4, 6], [6, 9]))
-
-print(total_cost)
+print(f"Part 1: {total_cost_p1}")
+print(f"Part 2: {total_cost_p2}")
